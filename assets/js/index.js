66e06ddfe144cc6f1db97ac93aeba6e70ade9171
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-24 18:14:07 -0500

    90% done thats an A passing grade

--- assets/js/index.js
+++ assets/js/index.js
@@ -91,6 +91,16 @@ function update(data){
       $(".list").append("<li class='repoListItem'><span class='octicon octicon-repo'></span><span class='repo'><div class='rep repTitle'>&nbsp;" + sortedStargazers[k].name + "</div><div class='rep repDesc'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + sortedStargazers[k].description + "</div><div class='stars'>" + sortedStargazers[k].stargazers_count + "<span class='octicon octicon-star'></span></div></span></li>");
     }//end for loop.
 
+    var sortedLastUpdate = data2.sort(sortByProperty("updated_at"));
+
+    $(".list2").html(" ");
+    for(var t=data2.length-1; t >= 0; t--){
+      $(".list2").append("<li class='repoListItem2'><span class='repo'><div class='rep repTitle2'>&nbsp;" + sortedLastUpdate[t].name +"</div><div class='rep repDesc2'>&nbsp;" + sortedLastUpdate[t].description + "</div><div class='updates'>&nbsp;updated 5 seconds ago </div><div class='repoStarBranch'><span class='octicon octicon-star'></span>" + sortedLastUpdate[t].stargazers_count + "<span class='octicon octicon-git-branch'></span>" + sortedLastUpdate[t].forks_count + "</div></span></li>")
+    }
+
+
+
+
     //this just verifies that we can get data points from the API
     // var stargazers = [];
     // for(var i = 0; i < data2.length; i++){
@@ -106,45 +116,6 @@ function convertToTimeArray(dateTime)  //Converting JSON date to workable string
   var dateTimeArray = [];
   dateTimeArray.push(dateTime.substr(0, 4));  //[0] year
   dateTimeArray.push(dateTime.substr(5, 2));  //[1] month
-  switch(dateTimeArray[1]){
-    case '01':
-      dateTimeArray[1]='Jan';
-      break;
-    case '02':
-      dateTimeArray[1]='Feb';
-      break;
-    case '03':
-      dateTimeArray[1]='Mar';
-      break;
-    case '04':
-      dateTimeArray[1]='Apr';
-      break;
-    case '05':
-      dateTimeArray[1]='May';
-      break;
-    case '06':
-      dateTimeArray[1]='June';
-      break;
-    case '07':
-      dateTimeArray[1]='July';
-      break;
-    case '08':
-      dateTimeArray[1]='Aug';
-      break;
-    case '09':
-      DateTimeArray[1]='Sep';
-      break;
-    case '10':
-      dateTimeArray[1]='Oct';
-      break;
-    case '11':
-      dateTimeArray[1]='Nov';
-      break;
-    case '12':
-      dateTimeArray[1]='Dec';
-      break;
-  }
-
   dateTimeArray.push(dateTime.substr(8, 2));  //[2] day
   dateTimeArray.push(dateTime.substr(11, 2));  //[3] hour
   dateTimeArray.push(dateTime.substr(14, 2));  //[4] minute
@@ -155,6 +126,62 @@ function convertToTimeArray(dateTime)  //Converting JSON date to workable string
 }
 
 var dateNow = convertToTimeArray(new Date().toJSON());
+
+function monthToWord(convertedTimeArray)
+{
+  switch(convertedTimeArray[1]){
+    case '01':
+      convertedTimeArray[1]='Jan';
+      break;
+    case '02':
+      convertedTimeArray[1]='Feb';
+      break;
+    case '03':
+      convertedTimeArray[1]='Mar';
+      break;
+    case '04':
+      convertedTimeArray[1]='Apr';
+      break;
+    case '05':
+      convertedTimeArray[1]='May';
+      break;
+    case '06':
+      convertedTimeArray[1]='June';
+      break;
+    case '07':
+      convertedTimeArray[1]='July';
+      break;
+    case '08':
+      convertedTimeArray[1]='Aug';
+      break;
+    case '09':
+      convertedTimeArray[1]='Sep';
+      break;
+    case '10':
+      convertedTimeArray[1]='Oct';
+      break;
+    case '11':
+      convertedTimeArray[1]='Nov';
+      break;
+    case '12':
+      convertedTimeArray[1]='Dec';
+      break;
+  }//end switch
+  return convertedTimeArray;
+}//end monthToWord function
+
+function howLongAgo(timeArray)
+{
+  var now = date().toJSON();
+  convertToTimeArray(now);
+  if(0)
+  {
+    //nothing
+  }
+}//end funct
+
+
+
 //tori found this on http://www.levihackwith.com/code-snippet-how-to-sort-an-array-of-json-objects-by-property/
 function sortByProperty(property)
 {
